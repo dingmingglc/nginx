9627f9db096dcdca455239f7bc4bad4f0a61702c
dingmingglc <dingming@baoding-soft.com>
2021-05-17 10:53:39 +0800

    Update ngx_file.c

--- src/core/ngx_file.c
+++ src/core/ngx_file.c
@@ -507,7 +507,8 @@ ngx_conf_set_access_slot(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
             goto invalid;
         }
 
-           ngx_log_debug1(NGX_LOG_EMERG, cf, 0, "22222 \"%V\" ", &value[i]);
+           ngx_log_debug1(NGX_LOG_EMERG, cf, 0, "22222333 ");
+        // ngx_log_debug1(NGX_LOG_EMERG, cf, 0, "22222 \"%V\" ", &value[i]);
           /* ngx_log_debug1(NGX_LOG_DEBUG_CORE, file->log, 0,"hashed path: %V", &value[i]); */ 
         
         
